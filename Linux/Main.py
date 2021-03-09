68bc61403c1b83753e21f049d70afd1232c14342
kty0205 <kimtaeyoung0205@naver.com>
2021-03-09 22:05:21 +0900

    많은 변화

---
+++ Linux/Main.py
@@ -0,0 +1,81 @@
+#Main.py for Linux
+
+try:
+	import time, os
+	from pgm import command as com
+	import subprocess as sp
+except:
+	with open('errorlog.txt', 'w') as errorlog:
+		errorlog.write('오류가 발생했습니다.\n')
+		errorlog.write('에러코드 : 001C\n')
+		errorlog.write('에러코드 읽는 방법을 참조해 주세요.\n')
+
+#메인
+while True:
+	import boot
+	boot.main()
+	from pgm import wordpad as wpad
+	from pgm import calc
+	from pgm import checkhw as chw
+	
+	chw.writefile()
+
+	while True:
+		locate = os.getcwd()
+		ans = input('\n' + locate + '>')
+		
+		#명령어
+		if ans == 'clear':
+			com.clear()
+		elif ans == 'help':
+			com.help()
+		elif ans == 'info':
+			com.info()
+		elif ans == 'hwinfo':
+			com.hwinfo()
+		elif 'cd' in ans:
+			com.cd(ans)
+		elif 'rmdir' in ans:
+			com.rmdir(ans)
+		elif 'mkdir' in ans:
+			com.mkdir(ans)
+		elif ans == 'shutdown':
+			com.shutdown()
+		elif 'rename' in ans:
+			com.rename(ans)
+		elif 'rmfile' in ans:
+			com.rmfile(ans)
+		elif ans == 'JSM_easter':
+			com.easter()
+		elif ans == 'filelist':
+			com.filelist()
+		elif ans == 'delcache':
+			com.delcache()
+		elif 'jpio' in ans:
+			com.jpio(ans)
+		
+		#문서편집기 실행
+		elif 'wpad ' in ans:
+			if 'new' in ans:
+				wpad.new()
+			else:
+				wpad.file_load(ans)
+		elif ans == 'wpad':
+			wpad.main()
+		
+		#계산기 실행
+		elif 'calc ' in ans:
+			calc.comcalc(ans)
+		elif ans == 'calc':
+			calc.main()
+		
+		#프로그램 실행
+		elif ans in os.listdir():
+			try:
+				cmd = 'python3'+ ' ' + ans
+				sp.run([cmd], shell=True)
+			except:
+				print('오류가 일어났습니다.')
+
+		else:
+			print(ans, '은(는) 명령어 또는 프로그램이 아닙니다.')
---
+++ Linux/readme.txt
@@ -0,0 +1,8 @@
+=====Codename BINGO=====
+BINGO는 BINGO Is Not GUI OS의 약자로 Wine의 Wine Is Not Emulator를 패러디해 지었습니다.
+
+압축을 /home/사용자이름에 풀어서 /home/사용자이름/OS_Linux가 되게 해주세요.
+
+터미널에 'sudo python3 /home/사용자이름/OS_Linux/Main.py'를 실행하세요.
+
+root권한으로 실행해야 정상적으로 실행됩니다.
