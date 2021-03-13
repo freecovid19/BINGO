e3ee8be461a600126b69b8ea18ff353c34caedee
김태영 <78518941+kty0205@users.noreply.github.com>
2021-03-13 21:10:45 +0900

    Add files via upload

--- Installer.py
+++ Installer.py
@@ -1,5 +1,5 @@
 '''
-installer.py v0.1
+installer.py v0.2
 참여하셨다면 이름을 입력해주세요
 1. 진성민
 2. forgot09
@@ -8,7 +8,11 @@ installer.py v0.1
 Windows, Linux. Android(선택)
 '''
 
-import requests, os, platform, time, zipfile, shutil
+import subprocess
+try:
+	import requests, os, platform, time, zipfile, shutil
+except:
+	pip('requests')
 
 def download(url, file_name = None):
 	if not file_name:
@@ -22,8 +26,14 @@ def extrec(zfile, path):
 	file.extractall(path)
 	file.close()
 
+def pip(module):
+	cmd = 'pip install ' + module
+  print(module, '을(를) 설치합니다.')
+	subprocess.run([cmd], shell=True)
+
 print('===[BINGO 설치 프로그램]===')
 print('사용자의 컴퓨터에 자동으로 BINGO를 설치합니다.')
+print('주의! 설치되어있던 BINGO는 자동삭제됩니다!')
 print('관리자권한(Root)으로 실행하세요.\n')
 
 useros = platform.system()
@@ -46,10 +56,12 @@ elif useros == 'Linux':
 	print('사용자이름을 입력하세요.')
 	user = str(input('Installer>'))
 	path = '/home' + '/' + user
-elif useros == 'Darwin' or 'darwin':
+elif useros == 'Darwin':
 	print('죄송합니다, macOS는 지원하지 않습니다.')
 	time.sleep(5)
 	quit()
+elif useros == 'Android':
+	pass
 
 if useros == 'Windows':
 	print('초기화중...')
@@ -68,6 +80,7 @@ if useros == 'Windows':
 	extrec(file, path)
 	os.rename('C:\\BINGO-Windows', 'C:\\OS_Windows')
 	os.remove('C:\\Windows.zip')
+  print('필요한 모듈 설치중...')
 elif useros == 'Linux':
 	try:
 		shutil.rmtree(path+'/BINGO-Linux')
@@ -87,6 +100,7 @@ elif useros == 'Linux':
 		direc.write(path+'/OS_Linux')
 	os.remove(path+'/Linux.zip')
 elif useros == 'Android':
+	print('초기화중...')
 	try:
 		shutil.rmtree('/storage/emulated/0/OS_Android')
 	except:
